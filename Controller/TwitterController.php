21e88da53f7bf1c3d3d5b4e1584a817e06eff3c7
Mark Lewis <thnkloud9@gmail.com>
2013-10-21 15:24:21 -0300

    changed from zend to symfony cache

--- Controller/TwitterController.php
+++ Controller/TwitterController.php
@@ -15,22 +15,15 @@ class TwitterController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $fOpts = array(
-            'lifetime' => 300,
-        );
-
-        $bOpts = array(
-            'cache_dir' => APPLICATION_PATH . '/../cache/'
-        );
-
-        $cache = Zend_Cache::factory('Core', 'File', $fOpts, $bOpts);
         $cache_key = md5("__twitterplugin_cache_user_timeline");
 
-        if (!$json = $cache->load($cache_key)) {
+        if ($jsonString = $this->get('cache')->fetch($cache_key)) {
+            $json = unserialize($jsonString);
+        } else {
             $twitterClient = $this->container->get('guzzle.twitter.client');
             $json = $twitterClient->get('statuses/user_timeline.json')
              ->send()->getBody();
-            $cache->save($json, $cache_key);
+            $this->get('cache')->save($cache_key, serialize($json));
         }
 
         $twitterFeed = json_decode($json, true);
